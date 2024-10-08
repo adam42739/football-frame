a37bce688243563588139c51bbb66471cdc76ef9
Adam Lynch <adamclynch03@gmail.com>
2024-10-08 16:50:12 -0400

    docs: stats package docstring

--- footballframe/stats/__init__.py
+++ footballframe/stats/__init__.py
@@ -1,3 +1,55 @@
+"""
+==============
+Football Stats
+==============
+
+The `stats` subpackage allows for keeping track of player and team stats while simulating. Use the `stats.Frame()` class for general stat keeping.
+
+Available Submodules
+--------------------
+
+Categories:
+
+>>> import stats.categories as categories
+
+Defense:
+
+>>> import stats.defense as defense
+
+Kicking:
+
+>>> import stats.kicking as kicking
+
+Passing:
+
+>>> import stats.passing as passing
+
+Punting:
+
+>>> import stats.punting as punting
+
+Receiving:
+
+>>> import stats.receiving as receiving
+
+Returning:
+
+>>> import stats.returning as returning
+
+Rushing:
+
+>>> import stats.rushing as rushing
+
+Scoring:
+
+>>> import stats.scoring as scoring
+
+Team:
+
+>>> import stats.team as team
+
+"""
+
 from . import categories
 from . import defense
 from . import kicking
