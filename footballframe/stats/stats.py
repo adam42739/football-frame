a856211d9a9f2d19ea51f6ef20b28cb1597a5063
Adam Lynch <adamclynch03@gmail.com>
2024-10-08 15:11:56 -0400

    feat: stats aggregating

--- footballframe/stats/stats.py
+++ footballframe/stats/stats.py
@@ -1,13 +1,19 @@
 import pandas
+from . import categories
+from . import types
 
 
 class Frame:
     def __init__(self):
+        self.category = None
         self.frame_dict = None
-        self.df = None
+        self.df = pandas.DataFrame
 
-    def define(self, headers: list[str]):
+    def define(self, category: str, headers: list[str] = None):
+        if not headers:
+            headers = categories.STAT_DICTS[category]
         self.frame_dict = {header: [] for header in headers}
+        self.category = category
 
     def create_df(self):
         if self.frame_dict:
@@ -26,5 +32,40 @@ class Frame:
                     self.frame_dict[header].append(None)
         else:
             raise Exception(
-                "Attempted to append to frame_dict but it does not exist. Call Frame.define()."
+                "Attempted to append to Frame.frame_dict but it does not exist. Call Frame.define()."
+            )
+
+    def _agg_accum(self):
+        for header in self.df.columns:
+            if categories.STAT_DICTS[self.category][header][types.TYPE] == types.ACCUM:
+                agg_val = self.df[header].sum()
+                self.aggregated[header] = agg_val
+
+    def _agg_other(self):
+        for header in self.df.columns:
+            if categories.STAT_DICTS[self.category][header][types.TYPE] == types.AVG:
+                X = 0
+                for X_header in categories.STAT_DICTS[self.category][header][
+                    types.PARAM
+                ]["X"]:
+                    X += self.aggregated[X_header]
+                Y = 0
+                for Y_header in categories.STAT_DICTS[self.category][header][
+                    types.PARAM
+                ]["Y"]:
+                    Y += self.aggregated[Y_header]
+                agg_val = X / Y
+                self.aggregated[header] = agg_val
+            elif categories.STAT_DICTS[self.category][header][types.TYPE] == types.MAX:
+                agg_val = self.df[header].max()
+                self.aggregated[header] = agg_val
+
+    def agg_df(self):
+        if not self.df.empty:
+            self.aggregated = {}
+            self._agg_accum()
+            self._agg_other()
+        else:
+            raise Exception(
+                "Attempted to aggregate Frame.df but it does not exist. Call Frame.create_df()."
             )
