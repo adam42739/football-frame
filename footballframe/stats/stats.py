8cdcca2a2acd6773893a056ced5d3eee299b01dc
Adam Lynch <adamclynch03@gmail.com>
2024-10-08 16:49:36 -0400

    docs: Frame class docstrings

--- footballframe/stats/stats.py
+++ footballframe/stats/stats.py
@@ -4,18 +4,66 @@ from . import types
 
 
 class Frame:
+    """
+    ===========
+    Stats.Frame
+    ===========
+
+    A class for general football stat keeping.
+
+    Usage
+    -----
+
+    First create and define a frame by category. Mixing stats from multiple categories into a single frame is not recommended.
+
+    >>> frame = Frame()
+    >>> frame.define(categories.PASSING)
+
+    Append rows of stats (typically after each play) to the frame
+
+    >>> frame.append({passing.CMP: 1, passing.ATT: 1, passing.YDS: 10})
+
+    Create a `pandas.DataFrame` from the `Frame` object. Creating a `pandas.DataFrame` is time consuming and should not be done frequently.
+
+    >>> frame.create_df
+
+    Aggregate all rows of the frame into totals/summary stats.
+
+    >>> frame.agg_df()
+
+    Access the aggregated summary stats.
+
+    >>> frame.aggregated
+
+    """
+
     def __init__(self):
         self.category = None
         self.frame_dict = None
         self.df = pandas.DataFrame
 
     def define(self, category: str, headers: list[str] = None):
+        """
+        Define the stats included in the `Frame`.
+
+        Parameters
+        ----------
+
+        category : str
+            The stats category to create the `Frame` for.
+
+        headers : list[str] = None
+            Provide if wishing to only include specific stats from the provided category. Including stats from other categories is not recommended.
+        """
         if not headers:
             headers = categories.STAT_DICTS[category]
         self.frame_dict = {header: [] for header in headers}
         self.category = category
 
     def create_df(self):
+        """
+        Create a `pandas.DataFrame` from associated with the `Frame`. Accessible as `Frame.df`.
+        """
         if self.frame_dict:
             self.df = pandas.DataFrame(self.frame_dict)
         else:
@@ -24,6 +72,25 @@ class Frame:
             )
 
     def append(self, row: dict[str, float]):
+        """
+        Append a row of stats to the `Frame`.
+
+        Parameters
+        ----------
+
+        row : dict[str, float]
+            A dictionary containing stats to append to the `Frame`.
+            If a stat is present in the `Frame` but not in `row`, `None` is used in place of the missing value.
+            Typically called after each play.
+
+        Example Usage
+        -------------
+
+        After a play consisting of a 10 yard completion. Update to the passer's stats:
+
+        >>> frame.append({passing.CMP: 1, passing.ATT: 1, passing.YDS: 10})
+
+        """
         if self.frame_dict:
             for header in self.frame_dict:
                 if header in row:
@@ -61,6 +128,9 @@ class Frame:
                 self.aggregated[header] = agg_val
 
     def agg_df(self):
+        """
+        Aggregate the `Frame` to create a `dict` of totals/summary stats. Aggregated `dict` accessible as `frame.aggregated`.
+        """
         if not self.df.empty:
             self.aggregated = {}
             self._agg_accum()
